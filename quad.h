071b2ab082ffc99ce42f92be682938479c235877
yasserbelhimer <yasserbelhimer@gmail.com>
2021-06-15 08:28:34 +0100

    set the quad in a file instead of the console

--- quad.h
+++ quad.h
@@ -10,12 +10,24 @@ void insererQuadr(char opr[],char op1[],char op2[],char res[])
 
 
 void AfficherQuadruples(){
-    printf("**   les quadruples **\n\n");
-    printf("-------------------------------------------\n");
-    for (int i = 0; i < qc; i++)
+    FILE *fichier = NULL;
+    fichier = fopen("quad.log", "w");
+    if (fichier != NULL)
     {
-        printf("%d - (%5s,%5s,%5s,%5s)\n",i,quadruples[i].oper,quadruples[i].ope1,quadruples[i].ope2,quadruples[i].res);
+        fprintf(fichier, "\n/**   les quadruples  **/\n\n");
+        fprintf(fichier, "-------------------------------------------------------------------\n");
+        fprintf(fichier, "|  N  |  Operateur   |   Operand 1  |   Operand 2  |   Resultat   |\n");
+        for (int i = 0; i < qc; i++)
+        {
+            char tempo[5];
+            sprintf(tempo,"%d",i);
+            fprintf(fichier, "|-----|--------------|--------------|--------------|--------------|\n");
+            fprintf(fichier, "|%3s  | %7s      | %7s      | %7s      | %7s      |\n",tempo,quadruples[i].oper,quadruples[i].ope1,quadruples[i].ope2,quadruples[i].res);
+        }
+        fprintf(fichier, "-------------------------------------------------------------------\n\n");
     }
-    printf("-------------------------------------------\n");
-    
+    else
+        printf("Impossible d'ouvrir le fichier ts.log\n");
+
+    fclose(fichier);
 }
